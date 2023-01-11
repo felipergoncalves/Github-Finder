37e3324a1f6b787239ae3a0633d17a3a237d3eeb
Felipe <feferodrigues771@gmail.com>
2023-01-11 16:32:19 -0300

    Design updates

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,5 @@
 import { UserProps } from "../types/user"
 import Search from "../components/Search"
-
 import {useState} from 'react'
 
 const Home = () => {
@@ -8,10 +7,22 @@ const Home = () => {
     const loadUser = async(userName: string) => {
         const res = await fetch(`https://api.github.com/users/${userName}`)
         const data = await res.json()
-        console.log(data)
-    }
+        const {avatar_url, login, location, followers, following} = data
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+        };
+        setUser(userData)
+    };
     return(
-        <Search loadUser = {loadUser}/>
+        <div>
+            <Search loadUser = {loadUser}/>
+            {user && <p>{user.login}</p>}
+        </div>
     )
 }
 
