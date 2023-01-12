56addac776e36afba940d5faeeb552ece6a20cc8
Felipe <feferodrigues771@gmail.com>
2023-01-12 18:33:07 -0300

    User section design

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,6 +1,7 @@
 import { UserProps } from "../types/user"
 import {MdLocationPin} from 'react-icons/md'
 import { Link } from "react-router-dom"
+import classes from './User.module.css'
 const User = ({
     login,
     avatar_url,
@@ -8,23 +9,23 @@ const User = ({
     following,
     location}: UserProps) => {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
             {location && (
-                <p>
+                <p className={classes.location}>
                     <MdLocationPin />
                     <span>{location}</span>
                 </p>
             )}
-            <div>
+            <div className={classes.stats}>
                 <div>
                     <p>Followers</p>
-                    <p>{followers}</p>
+                    <p className={classes.number}>{followers}</p>
                 </div>
                 <div>
                     <p>Following</p>
-                    <p>{following}</p>
+                    <p className={classes.number}>{following}</p>
                 </div>
             </div>
             <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
